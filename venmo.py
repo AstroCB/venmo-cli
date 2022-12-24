f73b4f4caa6ccdf9044cbfb23700dc3c1a2159f3
Cameron Bernhardt <cambernhardt@me.com>
2022-12-24 01:11:48 -0500

    Add logout command

--- venmo.py
+++ venmo.py
@@ -24,27 +24,26 @@ class Venmo:
             parser.print_help()
             exit(1)
 
-        self.client = Venmo._get_client()
+        self.client = self._get_client()
 
         getattr(self, args.subcommand)()
 
-    def _get_client():
-        access_token = ""
+    def _get_client(self):
         if os.path.exists(TOKEN_PATH):
             # Existing cached access token
             with open(TOKEN_PATH, "rb") as f:
-                access_token = pickle.load(f)
+                self.access_token = pickle.load(f)
         else:
             # Fresh login
             user = input("Venmo email: ")
             pwrd = getpass.getpass("Venmo password: ")
-            access_token = Client.get_access_token(username=user, password=pwrd)
+            self.access_token = Client.get_access_token(username=user, password=pwrd)
 
             # Cache access token
             with open(TOKEN_PATH, "wb") as f:
-                pickle.dump(access_token, f)
+                pickle.dump(self.access_token, f)
 
-        return Client(access_token=access_token)
+        return Client(access_token=self.access_token)
 
     def _get_subargs(parser):
         # ignore the executable itself and the subcommand
@@ -57,6 +56,21 @@ class Venmo:
             print("User not found")
             exit(1)
         return user
+
+    def logout(self):
+        parser = argparse.ArgumentParser(
+            description="""
+Log out of your current session; your session will never expire unless you log out
+"""
+        )
+        Venmo._get_subargs(parser)  # handle -h even though we don't use any args
+
+        if not self.client.log_out(self.access_token):
+            print("Failed to log out of Venmo session")
+            exit(1)
+
+        os.remove(TOKEN_PATH)
+        print("Successfully logged out of Venmo session")
 
     def request(self):
         parser = argparse.ArgumentParser(description="Request money from another user")
